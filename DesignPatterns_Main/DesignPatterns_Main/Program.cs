51036810301b14d4b6242601aca75b54822d437a
Armando Castañeda Tenango <armando_ct83@hotmail.com>
2020-05-11 17:18:00 -0500

    Se crea el proyecto main y el proyecto de Factory Method

---
+++ DesignPatterns_Main/DesignPatterns_Main/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using FactoryMethod;
+
+namespace DesignPatterns_Main
+{
+    public class Program
+    {
+        #region Properties_Definition
+
+        private static string[] _options = { "Salir","Factory Method"};
+
+        private static Action[] functions = { () => factoryMethod() };
+        //, () => insertDL(), () => pancakeSorting(),
+        //    ()=> fibonacciIter(),()=>fibonacciRecursivo(),()=> inserMaxHeap(),()=>sortingMaxHeap(),()=>LinkedList(),()=>Bubble(),()=>removeDuplicatedSET(),
+        //()=>treeExample()};
+
+
+        private const int DOTS_EDGE = 7;
+        #endregion
+        
+        static void Main(string[] args)
+        {
+            ShowMenu();
+            var optionSelected = readOption();
+            while (optionSelected > 0)
+            {
+                switch (optionSelected)
+                {
+                    case 1:
+                        functions[0].Invoke();
+                        break;
+                    case 2:
+                        functions[1].Invoke();
+                        break;
+                    case 3:
+                        functions[2].Invoke();
+                        break;
+                    case 4:
+                        functions[3].Invoke();
+                        break;
+                    case 5:
+                        functions[4].Invoke();
+                        break;
+                    case 6:
+                        functions[5].Invoke();
+                        break;
+                    case 7:
+                        functions[6].Invoke();
+                        break;
+                    case 8:
+                        functions[7].Invoke();
+                        break;
+                    case 9:
+                        functions[8].Invoke();
+                        break;
+                    case 10:
+                        functions[9].Invoke();
+                        break;
+                    case 11:
+                        functions[10].Invoke();
+                        break;
+                    default:
+                        break;
+                }
+                ShowMenu();
+                optionSelected = readOption();
+            }
+        }
+
+
+        public static decimal factoryMethod()
+        {
+            var factoryMethod = new FactoryMethod.FactoryMethod();
+            factoryMethod.Run();
+            return 0;
+        }
+
+        #region Menu_functions
+        public static void ShowMenu()
+        {
+            Console.Clear();
+            var index = 0;
+            foreach (var option in _options)
+            {
+                if (index < 1)
+                    Console.WriteLine($"::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+                //else
+                //{
+                    for (int i = 0; i < DOTS_EDGE; i++)
+                    {
+                        Console.Write($" ");
+                    }
+                    Console.Write($"{index} .- {option}");
+                    for (int i = 0; i < DOTS_EDGE; i++)
+                    {
+                        Console.Write($" ");
+                    }
+                    Console.Write(System.Environment.NewLine);
+                //}
+                index++;
+            }
+            Console.WriteLine($"::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        }
+
+        public static int readOption()
+        {
+            Console.Write($"Escribe el numero de tu opcion:: ");
+            string input = Console.ReadLine();
+            int number;
+            if (Int32.TryParse(input, out number))
+            {
+                return Int32.Parse(input);
+            }
+            else
+                return 0;
+        }
+        #endregion
+    }
+}
---
+++ DesignPatterns_Main/FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// Clase que lanza el escenario donde se implementa el Patron Factory Method.
+    /// 
+    /// Armando Castaneda - Mayo 2020
+    /// </summary>
+    public class FactoryMethod
+    {
+        public FactoryMethod()
+        {
+            /*
+             TIPO  DE PATRON: Creacional (Tipo de patron utilizado para crear los objetos)
+             
+             OBJETIVO :: Factory Method permite la creacion de objetos de un subtipo determinado a traves de una clase Factory.
+
+             EJEMPLO :: Por ejemplo en un videojuego el usuario puede usar un joystick  o el teclado y pudiera ser que cada device
+             tiene un controlador diferente, aqui el patron desacopla el videojuego del control a utilizar.
+             
+             TIPS: Cuando tenemos varios IFs o SWITCH para crear objetos de cierto tipo.
+                   Cuando no sabemos en tiepo de diseño la subclase que se utilizará
+
+             ESCENARIO: Requerimos una conexion a un usuario poder trabajar con 2 Base de datos Diferentes, por ejemplo Oracle y MySQL
+             */
+        }
+
+        public void Run() {
+            Console.WriteLine("Iniciando ->  FactoryMethod ....");
+
+
+
+            Console.WriteLine("Finalizando ->  FactoryMethod ....");
+        }
+    }
+}
